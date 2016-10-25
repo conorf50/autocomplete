a246f0d5dc7e18c909f4ef215d778d3232032d52
conorf50 <conorf50@gmail.com>
2016-10-25 20:58:23 +0100

    Made some progress on GUI

--- src/cfarrell/Main.java
+++ src/cfarrell/Main.java
@@ -1,18 +1,50 @@
 package cfarrell;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.Scanner;
+
+import asg.cliche.Command;
+import asg.cliche.Param;
+import asg.cliche.Shell;
+import asg.cliche.ShellFactory;
+
+
+
 
 public class Main {
 
-	public static void main(String[] args) throws FileNotFoundException {
+	public static void main(String[] args) throws IOException {
 		BruteAutoComplete x = new BruteAutoComplete();
 		x.weightOf("hell");
 		x.bestMatch("yo");
-
+		
+		Main main = new Main();
+		Shell shell = ShellFactory.createConsoleShell("pm", "Welcome to autocomplete- ?help for instructions", main);
+		shell.commandLoop();
 	}
 	private BruteAutoComplete bruteComplete;
 	
 	
+	 @Command(description="Load in a file to process")
+	  public void loadFile (@Param(name="FileName") String fileName)
+	  {
+	   
+	    System.out.println("Work In Progress");
+	  }
+	 
+	 @Command(description="Enter a prefix")
+	  public void bestMatch (@Param(name="prefix") String fileName)
+	  {
+	   
+	    System.out.println("Work In Progress");
+	  }
+	 
+	 @Command(description="Load in a file to process")
+	  public void displayWords (@Param(name="topWords") String fileName)
+	  {
+	   
+	    System.out.println("Work In Progress");
+	  }
+	 
+	
+
 }
