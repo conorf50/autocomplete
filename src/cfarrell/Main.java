37316c207c6bfe07bbc8b9b683d84d75a18e23c5
conorf50 <conorf50@gmail.com>
2016-11-04 15:10:52 +0000

    Took out Cliche integration, was broken and not working

--- src/cfarrell/Main.java
+++ src/cfarrell/Main.java
@@ -3,23 +3,22 @@ package cfarrell;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Scanner;
-import asg.cliche.Command;
-import asg.cliche.Param;
-import asg.cliche.Shell;
-import asg.cliche.ShellFactory;
+//import asg.cliche.Command;
+//import asg.cliche.Param;
+//import asg.cliche.Shell;
+//import asg.cliche.ShellFactory;
 
 public class Main {
 	
 	private Scanner sc = new Scanner(System.in);
 	//called by interface name first
 	AutoComplete brute;    //brute force autocomplete
-	
 	AutoComplete quick;    //quick autocomplete
 	public static void main(String[] args) throws IOException {
 		Main main = new Main();
 		//Create a shell with ac@ and the user's name displayed in lower case
-		Shell shell = ShellFactory.createConsoleShell("ac@" + System.getProperty("user.name").toLowerCase(), "Welcome to autocomplete- ?help for instructions", main);
-		shell.commandLoop();
+		//Shell shell = ShellFactory.createConsoleShell("ac@" + System.getProperty("user.name").toLowerCase(), "Welcome to autocomplete- ?help for instructions", main);
+		//shell.commandLoop();
 	}
     
 	public Main()
@@ -32,31 +31,67 @@ public class Main {
 		}
 	}
 	
-	/* @Command(description="Load in a file to process")
-	  public void loadFile (@Param(name="FileName") String fileName)
+	
+	private int mainMenu()
+    {
+      System.out.println("1) Enter URL of list");
+      System.out.println("2) Enter term");       
+      System.out.println("3) Change autocomplete mode");
+      System.out.println("0) Exit");
+      System.out.print("==>>");
+      int option = sc.nextInt();
+      return option;
+    }
+     
+    public void run() {
+        {
+           System.out.println("Posts");
+           int option = mainMenu();
+           while (option != 0)
+           {
+            
+             switch (option)
+             {
+               case 1:               
+                 URLName();
+                 break;
+               case 2:
+                 matches(prefix,k);
+                 break;
+               case 3:
+                 showPosts();
+                 break;
+                }
+            
+             option = mainMenu();
+           }
+           System.out.println("Exiting... bye");
+        }
+    }
+	// @Command(description="Enter a prefix")
+	  public void matches ()
 	  {
-	    //loadFromFile();
-	  }*/
-	 
-	 @Command(description="Enter a prefix")
-	  public void enterPrefix (@Param(name="prefix") String prefix)
-	  {
-		String pre = sc.next();
-		int k = 0;
-	    brute.matches(pre, k);
+//		String prefix = sc.next();
+	    brute.matches(prefix, k);
 	    System.out.println("Work In Progress");
 	  }
 	 
-	 @Command(description="Load in a file to process")
-	  public void URLName (@Param(name="URLName") String fileName)
+	
+	// @Command(description="Load in a file to process")
+	  public void URLName ()
 	  {
+		 
 	    System.out.println("Work In Progress");
 	  }
 	 
-	 @Command(description="Allow the user to switch between quick and slow complete modes")
-	  public void mode (@Param(name="mode") String modeName)
+	// @Command(description="Allow the user to switch between quick and slow complete modes")
+	  public void mode ( String modeName)
 	  {
 		 boolean useQuickComplete = false;
+		 /*
+		  *include some method for setting this to true when the user puts in a value of say "1" for a quick search. 
+		  *Then return to the main menu and show that this has been selected
+		 */
 	    System.out.println("The only mode is BruteAutoComplete!");
 	  }
 	 
